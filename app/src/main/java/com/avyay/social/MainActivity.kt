0bbec8ff9a8bd6e87b09577672dabafea332cbcb
avyay kumar singh <shubhamsinghak1234@gmail.com>
2021-08-27 18:10:12 +0530

    Initial commit

---
+++ app/src/main/java/com/avyay/social/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.avyay.social
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.Query
+import daos.PostDao
+import kotlinx.android.synthetic.main.activity_main.*
+import models.Post
+
+class MainActivity : AppCompatActivity(), IPostAdapter {
+
+    private lateinit var postDao: PostDao
+    private lateinit var adapter: PostAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener {
+            val intent = Intent(this, CreatePostActivity::class.java)
+            startActivity(intent)
+        }
+
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        postDao = PostDao()
+        val postCollections = postDao.postCollection
+        val query = postCollections.orderBy("createdAt", Query.Direction.DESCENDING)
+        val recyclerViewOptions =
+            FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+
+
+        adapter = PostAdapter(recyclerViewOptions, this)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.startListening()
+    }
+
+    override fun onLikeClicked(postId: String) {
+        postDao.updateLikes(postId)
+    }
+}
---
+++ app/src/main/java/models/User.kt
@@ -0,0 +1,8 @@
+package models
+
+data class User(
+    val uid: String = "",
+    val displayName: String? = "",
+    val imageUrl: String = ""
+)
+
